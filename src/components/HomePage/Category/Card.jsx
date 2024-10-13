ac943cd9ce2c022f9786955608c39936422ef349
MH-Shawon <mohsinshawon18@gmail.com>
2024-10-14 00:13:26 +0600

    push

--- src/components/HomePage/Category/Card.jsx
+++ src/components/HomePage/Category/Card.jsx
@@ -5,18 +5,18 @@ import { useState } from "react";
 const Card = () => {
   const [cardHover, setCardHover] = useState("card1");
   return (
-    <div className="container flex flex-col gap-10 px-4 mx-auto mt-5 bg-white lg:flex-row sm:px-0 ">
+    <div className="container flex flex-col gap-10 mx-auto mt-5 bg-white lg:flex-row sm:px-0 ">
       {/* card-1 */}
       <div
-        className="relative w-full sm:w-96 lg:w-1/3 lg:h-[487px]  mx-auto "
+        className="relative  sm:w-96  lg:h-[487px]   "
         onMouseEnter={() => setCardHover("card1")}
         onMouseLeave={() => setCardHover(null)}
       >
         {cardHover === "card1" && (
-          <div className="absolute bottom-[-50px] left-[-50px] w-[120px] h-[120px] bg-[#244D4D] rounded-[46.548px_0px_15.516px_0px] z-0 transition-transform duration-300 ease-in-out"></div>
+          <div className="absolute bottom-5 left-[-30px] w-[120px] h-[120px] bg-[#244D4D] rounded-[46.548px_0px_15.516px_0px] z-0 transition-transform duration-300 ease-in-out"></div>
         )}
 
-        <div className="relative z-10 bg-white rounded-[55px] w-full sm:w-[350px] h-full hover:shadow-xl p-6 transition-transform transform duration-300 ease-in-out hover:scale-105">
+        <div className="relative z-10 bg-white rounded-[55px] w-[420px] sm:w-[350px] h-[420px] hover:shadow-xl p-6 transition-transform transform duration-300 ease-in-out hover:scale-105">
           <figure className="flex justify-center pt-6">
             <Image
               alt="honesty"
@@ -32,7 +32,8 @@ const Card = () => {
               Honesty
             </h2>
             <p className="font-baiJamjuree text-[#5E6282] text-[21px] leading-[40.341px] mt-4 hover:text-[#333E59] transition-colors duration-300">
-              We prioritize authenticity and follow a transparent, non-pushy approach.
+              We prioritize authenticity and follow a transparent, non-pushy
+              approach.
             </p>
           </div>
         </div>
@@ -45,10 +46,10 @@ const Card = () => {
         onMouseLeave={() => setCardHover(null)}
       >
         {cardHover === "card2" && (
-          <div className="absolute bottom-[-50px] left-[-50px] w-[120px] h-[120px] bg-[#244D4D] rounded-[46.548px_0px_15.516px_0px] z-0 transition-transform duration-300 ease-in-out"></div>
+          <div className="absolute bottom-5 left-[-30px] w-[120px] h-[120px] bg-[#244D4D] rounded-[46.548px_0px_15.516px_0px] z-0 transition-transform duration-300 ease-in-out"></div>
         )}
 
-        <div className="relative z-10 bg-white rounded-[55px] w-full sm:w-[350px] h-full hover:shadow-xl p-6 transition-transform transform duration-300 ease-in-out hover:scale-105">
+        <div className="relative z-10 bg-white rounded-[55px] w-[420px] sm:w-[350px] h-[420px] hover:shadow-xl p-6 transition-transform transform duration-300 ease-in-out hover:scale-105">
           <figure className="flex justify-center pt-6">
             <Image
               alt="honesty"
@@ -64,7 +65,8 @@ const Card = () => {
               Honesty
             </h2>
             <p className="font-baiJamjuree text-[#5E6282] text-[21px] leading-[40.341px] mt-4 hover:text-[#333E59] transition-colors duration-300">
-              We prioritize authenticity and follow a transparent, non-pushy approach.
+              We prioritize authenticity and follow a transparent, non-pushy
+              approach.
             </p>
           </div>
         </div>
@@ -77,10 +79,10 @@ const Card = () => {
         onMouseLeave={() => setCardHover(null)}
       >
         {cardHover === "card3" && (
-          <div className="absolute bottom-[-50px] left-[-50px] w-[120px] h-[120px] bg-[#244D4D] rounded-[46.548px_0px_15.516px_0px] z-0 transition-transform duration-300 ease-in-out"></div>
+          <div className="absolute bottom-5 left-[-30px] w-[120px] h-[120px] bg-[#244D4D] rounded-[46.548px_0px_15.516px_0px] z-0 transition-transform duration-300 ease-in-out"></div>
         )}
 
-        <div className="relative z-10 bg-white rounded-[55px] w-full sm:w-[350px] h-full hover:shadow-xl p-6 transition-transform transform duration-300 ease-in-out hover:scale-105">
+        <div className="relative z-10 bg-white rounded-[55px] w-[420px] sm:w-[350px] h-[420px] hover:shadow-xl p-6 transition-transform transform duration-300 ease-in-out hover:scale-105">
           <figure className="flex justify-center pt-6">
             <Image
               alt="honesty"
@@ -96,13 +98,13 @@ const Card = () => {
               Honesty
             </h2>
             <p className="font-baiJamjuree text-[#5E6282] text-[21px] leading-[40.341px] mt-4 hover:text-[#333E59] transition-colors duration-300">
-              We prioritize authenticity and follow a transparent, non-pushy approach.
+              We prioritize authenticity and follow a transparent, non-pushy
+              approach.
             </p>
           </div>
         </div>
       </div>
     </div>
-
   );
 };
 export default Card;
--- src/components/HomePage/Testimony.jsx
+++ src/components/HomePage/Testimony.jsx
@@ -1,89 +1,91 @@
 import Image from "next/image";
+import CommonLayout from "../layouts/commonLayout";
 
 const Testimony = () => {
   return (
-    <section className="flex flex-col justify-center lg:flex-row lg:items-center">
-      <div className="pl-12 space-y-8 lg:w-1/3">
-        <p className="text-sm font-poppins font-semibold tracking-wide text-[#5E6282] uppercase">
-          Testimonials
-        </p>
-        <h2 className="w-[320px] text-4xl font-bold capitalize text-primary font-volkhov">
-          What People Say About Us.
-        </h2>
+    <CommonLayout>
+      <section className="flex flex-col justify-between lg:flex-row lg:items-center">
+        <div className="space-y-8 text-start lg:w-1/3">
+          <p className="text-sm font-poppins font-semibold tracking-wide text-[#5E6282] uppercase">
+            Testimonials
+          </p>
+          <h2 className="w-[320px] text-4xl font-bold capitalize text-primary font-volkhov">
+            What People Say About Us.
+          </h2>
 
-        <div className="flex gap-2 mt-4">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="87"
-            height="12"
-            viewBox="0 0 87 12"
-            fill="none"
-          >
-            <circle cx="6" cy="6" r="6" fill="#39425D" />
-            <circle cx="44" cy="6" r="6" fill="#E5E5E5" />
-            <circle cx="81" cy="6" r="6" fill="#E5E5E5" />
-          </svg>
+          <div className="flex gap-2 mt-4">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="87"
+              height="12"
+              viewBox="0 0 87 12"
+              fill="none"
+            >
+              <circle cx="6" cy="6" r="6" fill="#39425D" />
+              <circle cx="44" cy="6" r="6" fill="#E5E5E5" />
+              <circle cx="81" cy="6" r="6" fill="#E5E5E5" />
+            </svg>
+          </div>
         </div>
-      </div>
 
-      {/* Testimonial Card */}
+        {/* Testimonial Card */}
 
-      <div className="relative mr-24 mt-28 text-[#5E6282] font-poppins">
-       
-        <div className="absolute w-[450px] h-[245px] rounded-lg bg-[#F7F7F7] border-2 -bottom-[67px] left-12">
-          <div className="relative w-full h-full">
-            <div className="absolute text-left bottom-5 left-8">
-              <h1 className="font-semibold text-[#5E6282]">Chris Thomas</h1>
-              <p className="text-sm text-[#5E6282]">CEO of Red Button</p>
+        <div className="relative mr-24 mt-28 text-[#5E6282] font-poppins">
+          <div className="absolute w-[450px] h-[245px] rounded-lg bg-[#F7F7F7] border-2 -bottom-[67px] left-12">
+            <div className="relative w-full h-full">
+              <div className="absolute text-left bottom-5 left-8">
+                <h1 className="font-semibold text-[#5E6282]">Chris Thomas</h1>
+                <p className="text-sm text-[#5E6282]">CEO of Red Button</p>
+              </div>
             </div>
           </div>
 
-        </div>
+          {/* Foreground Div (relative) */}
+          <div className="relative w-[450px] h-[245px] p-8 rounded-xl  bg-white shadow-xl mb-4">
+            <Image
+              src="/assets/images/testimoni.png"
+              alt="User Profile"
+              height={68}
+              width={68}
+              className="absolute w-16 h-16 border-2 border-white rounded-full shadow-sm -top-4 -left-4"
+            />
 
-        {/* Foreground Div (relative) */}
-        <div className="relative w-[450px] h-[245px] p-8 rounded-xl  bg-white shadow-xl mb-4">
-          <Image
-            src="/assets/images/testimoni.png"
-            alt="User Profile"
-            height={68}
-            width={68}
-            className="absolute w-16 h-16 border-2 border-white rounded-full shadow-sm -top-4 -left-4"
-          />
-
-          <p className="mt-8 mb-4 text-lg text-[#5E6282]">
-            “On the Windows talking painted pasture yet its express parties use.
-            Sure last upon he same as knew next. Or believed or diverted no.”
-          </p>
-          <div>
-            <p className="font-semibold text-[#5E6282]">Mike taylor</p>
-            <p className="text-sm text-[#5E6282]">Lahore, Pakistan</p>
+            <p className="mt-8 mb-4 text-lg text-[#5E6282]">
+              “On the Windows talking painted pasture yet its express parties
+              use. Sure last upon he same as knew next. Or believed or diverted
+              no.”
+            </p>
+            <div>
+              <p className="font-semibold text-[#5E6282]">Mike taylor</p>
+              <p className="text-sm text-[#5E6282]">Lahore, Pakistan</p>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="w-3 h-[71px] mt-24 ">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="75"
-          viewBox="0 0 16 75"
-          fill="none"
-        >
-          <path
-            d="M1 8.5L8 1.5L15 8.5"
-            stroke="#BCB7C2"
-            stroke-width="2"
-            stroke-linecap="round"
-          />
-          <path
-            d="M1 66L8 73L15 66"
-            stroke="#3E2E4D"
-            stroke-width="2"
-            stroke-linecap="round"
-          />
-        </svg>
-      </div>
-    </section>
+        <div className="w-3 h-[71px] mt-24 ">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="75"
+            viewBox="0 0 16 75"
+            fill="none"
+          >
+            <path
+              d="M1 8.5L8 1.5L15 8.5"
+              stroke="#BCB7C2"
+              stroke-width="2"
+              stroke-linecap="round"
+            />
+            <path
+              d="M1 66L8 73L15 66"
+              stroke="#3E2E4D"
+              stroke-width="2"
+              stroke-linecap="round"
+            />
+          </svg>
+        </div>
+      </section>
+    </CommonLayout>
   );
 };
 export default Testimony;
